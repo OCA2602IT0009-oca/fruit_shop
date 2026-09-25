6d4a3a5fee5ef1bf3022b0d5cbafff11476f6288
Jimin Song <OCA2602IT0009@edu.oca.ac.jp>
2026-09-25 03:08:59 +0000

    Initial commit (Rails setup)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,14 @@ class Product < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :price, presence: true
 
-    has_many :orders
+    has_many :orders, dependent: :destroy
+
+    # Active Storage（商品画像）
+    has_one_attached :photo
+
+    # 商品画像のサムネイルを生成
+    def thumbnail
+      photo.variant(resize_to_limit: [150, 150]).processed  # 画像を 150x150 ピクセル以内にリサイズ
+    end
+    
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # MiniMagick
+  Rails.application.config.active_storage.variant_processor = :mini_magick
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
